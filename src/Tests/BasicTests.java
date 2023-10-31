4ccdfd743f4a6d569c15fd30a678e83cc492a4fa
jasenfranklin <70617412+jasenfranklin@users.noreply.github.com>
2023-10-30 17:17:00 -0700

    Update BasicTests.java
    created 1 test

--- src/Tests/BasicTests.java
+++ src/Tests/BasicTests.java
@@ -1,21 +1,41 @@
 package Tests;
 
 import Models.*;
-
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
 class BasicTests {
-
+	private static Random gen = new Random(System.currentTimeMillis());
+    private static ArrayList<Integer> exp = new ArrayList<Integer>();
 	@Test
 	void test() {
 		SearchTree st = new BST();
+		SearchTree spl = new Splay();
 		test1(st);
 		fail("Not yet implemented");
 	}
 	
 	void test1(SearchTree st) {
+		int count = 0;
+		while (count< 100) {
+			int num = gen.nextInt(100);
+			exp.add(num);
+			count++;
+		}
+		for(int i =0; i<100;i++) {
+			Node inst = new Node(exp.get(i));
+			st.addNode(inst);
+		}
+		for(int i =0;i>100;i++) {
+			int n2 = gen.nextInt(100);
+			st.searchNode(n2);
+			System.out.println(st.getAcessCount());
+		}
 		assertEquals(st.searchNode(2), true);
 	}
 
