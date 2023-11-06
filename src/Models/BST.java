8a54f94e4dd1f9d0d3878675a8068ed045423baf
JoseZepeda0 <148504322+JoseZepeda0@users.noreply.github.com>
2023-11-06 15:44:50 -0700

    Update BST.java
    bst now works

--- src/Models/BST.java
+++ src/Models/BST.java
@@ -1,100 +1,61 @@
 package Models;
 
 public class BST implements SearchTree {
-	
-	private int accessCount = 0;
-
 	Node root;
 
 	public BST() {
 		root = null;
 	}
 
-	@Override
-	public void addNode(Node node) {
-		root = addNode(this.root, node);
+	public boolean addNode(Node n) {
+		root = addNodeRecursive(root, n.value);
+		return true;
 	}
 
-	private Node addNode(Node parent, Node node) {
-		accessCount++;
-		if (parent == null) {
-			parent = node;
+	private Node addNodeRecursive(Node current, int value) {
+		if (current == null) {
+			return new Node(value);
 		}
 
-		if (node.value < parent.value) {
-			parent.left = addNode(parent.left, node);
+		if (value < current.value) {
+			current.left = addNodeRecursive(current.left, value);
+		} else if (value > current.value) {
+			current.right = addNodeRecursive(current.right, value);
 		}
 
-		if (node.value > parent.value) {
-			parent.right = addNode(parent.right, node);
-		}
-
-		return parent;
+		return current;
 	}
 
-	@Override
 	public boolean searchNode(int value) {
-		return searchNode(this.root, value);
+		return searchNodeRecursive(root, value) != null;
 	}
 
-	private boolean searchNode(Node root, int value) {
-		accessCount ++;
-		if (root == null)
-			return false;
-		if (value == root.value)
-			return true;
-		if (value < root.value)
-			return searchNode(root.left, value);
-		if (value > root.value)
-			return searchNode(root.right, value);
-		return false;
+	private Node searchNodeRecursive(Node current, int value) {
+		if (current == null)
+			return null;
+
+		if (current.value == value) {
+			return current;
+		}
+
+		if (value < current.value) {
+			return searchNodeRecursive(current.left, value);
+		}
+
+		return searchNodeRecursive(current.right, value);
 	}
 
 	@Override
 	public int getAcessCount() {
-		return accessCount;
-
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
 	@Override
 	public int clearAcessCount() {
-		int temp = accessCount;
-		accessCount = 0;
-		return temp;
+		// TODO Auto-generated method stub
+		return 0;
 
-	}
-	
-	public void log() {
-		System.out.println("Access Count: " + accessCount);
-		log(root);
-	}
-	
-	private static void log(Node root) {
-		if(root == null) {
-			// do nothing
-		}else {
-			if(root.left != null) {
-				log(root.left);
-			}
-			System.out.print(root.value + ", ");
-			if(root.right != null) {
-				log(root.right);
-			}
-		}
-	}
-	
-	public static void main(String[] args) {
-		BST s = new BST();
-		s.addNode(new Node(1));
-		s.addNode(new Node(2));
-		s.addNode(new Node(3));
-		s.addNode(new Node(0));
-		s.addNode(new Node(-2));
-		
-		s.log();
-		System.out.println("");
-		
-		System.out.println(s.searchNode(3));
 	}
 
 }
