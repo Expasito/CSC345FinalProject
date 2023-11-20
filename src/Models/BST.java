1d7fb3325605e63996626b88caf1a290a9b7e691
JoseZepeda0 <148504322+JoseZepeda0@users.noreply.github.com>
2023-11-20 16:30:38 -0700

    Update BST.java
    documentation is added

--- src/Models/BST.java
+++ src/Models/BST.java
@@ -1,18 +1,50 @@
+/**
+ * filename: BST.java
+ * 
+ * implements a binary search tree with an accessCount in order to test and compare efficiency against splay trees
+ * there are no duplicates and there is no deletion
+ * 
+ * course:	CSC 345
+ * project:	Team Project
+ * date:	11/20/2023
+ */
+
 package Models;
 
 public class BST implements SearchTree {
+	// root node of the tree
 	Node root;
-	
+
+	// counts how many times the nodes of the tree have been accessed
 	private int accessCount = 0;
 
+	// constructor
 	public BST() {
 		root = null;
 	}
 
+	// constructor
+	public BST(Node node) {
+		root = node;
+	}
+
+	/**
+	 * Adds node n to the tree
+	 * 
+	 * @param n n is the node we are adding
+	 * @return void
+	 */
 	public void addNode(Node n) {
 		root = addNodeRecursive(root, n.value);
 	}
 
+	/**
+	 * Helper function for adding a node
+	 * 
+	 * @param current is the current node that we are on
+	 * @param value   is the value of the original node we are adding
+	 * @return returns current node in order to recurse
+	 */
 	private Node addNodeRecursive(Node current, int value) {
 		accessCount++;
 		if (current == null) {
@@ -28,10 +60,24 @@ public class BST implements SearchTree {
 		return current;
 	}
 
+	/**
+	 * Searches for the node that has the value value, and returns true if it is
+	 * found
+	 * 
+	 * @param value value we are going to search for
+	 * @return returns if the node was found
+	 */
 	public boolean searchNode(int value) {
 		return searchNodeRecursive(root, value) != null;
 	}
 
+	/**
+	 * Helper function in for searching a node
+	 * 
+	 * @param current current node we are in
+	 * @param value   the value we are searching for
+	 * @return current node in order to recurse
+	 */
 	private Node searchNodeRecursive(Node current, int value) {
 		if (current == null)
 			return null;
@@ -49,50 +95,25 @@ public class BST implements SearchTree {
 	}
 
 	@Override
+	/**
+	 * returns the trees access count
+	 * 
+	 * @return the access count of the tree
+	 */
 	public int getAcessCount() {
 		return accessCount;
 	}
 
 	@Override
+	/**
+	 * resets the access count and returns previous one
+	 * 
+	 * @return Previous access count
+	 */
 	public int clearAcessCount() {
 		int temp = accessCount;
 		accessCount = 0;
 		return temp;
-
-	}
-	
-	public void log() {
-		System.out.println("Access Count: " + accessCount);
-		log(root);
-	}
-	
-	private static void log(Node root) {
-		if(root == null) {
-			// do nothing
-		}else {
-			if(root.left != null) {
-				log(root.left);
-			}
-			System.out.print(root.value + ", ");
-			if(root.right != null) {
-				log(root.right);
-			}
-		}
-	}
-	
-	public static void main(String[] args) {
-		BST s = new BST();
-		s.addNode(new Node(1));
-		s.addNode(new Node(2));
-		s.addNode(new Node(3));
-		s.addNode(new Node(0));
-		s.addNode(new Node(-2));
-		
-		s.log();
-		System.out.println("");
-		
-		System.out.println(s.searchNode(3));
-//		System.out.println(s.root);
 	}
 
 }
