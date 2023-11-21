6ab68c54fd82f2ff3e04a29e7b876b77ca936c51
jasenfranklin <70617412+jasenfranklin@users.noreply.github.com>
2023-11-21 15:47:09 -0700

    Add files via upload
    created another test case

--- src/Tests/BasicTests.java
+++ src/Tests/BasicTests.java
@@ -1,13 +1,16 @@
 package Tests;
 
-import Models.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+
+import Models.BST;
+import Models.Node;
+import Models.SearchTree;
+import Models.Splay;
 
 class BasicTests {
 	private static Random gen = new Random(System.currentTimeMillis());
@@ -16,23 +19,25 @@ class BasicTests {
 	void test() {
 		SearchTree st = new BST();
 		SearchTree spl = new Splay();
-		//test1(st);
-		test2(st, spl);
+		SearchTree st2 = new BST();
+		SearchTree spl2 = new Splay();
+		SearchTree st3 = new BST();
+		SearchTree spl3 = new Splay();
+		SearchTree spl4 = new Splay();
+		SearchTree st4=  new BST();
+		SearchTree st5 = new BST();
+		SearchTree spl5 = new Splay();
+		test1(st, spl);
+		test2(st2, spl2);
+		test3(st3, spl3);
+		test4(st4, spl4);
+		test5(st5, spl5);
 //		fail("Not yet implemented");
 
 	}
 	
-	@Test
-	void random_nonrandom() {
-		SearchTree bst = new BST();
-		SearchTree splay = new Splay();
-		System.out.println("\n\nTesting BST");
-		test3(bst, 100, 5, 1000);
-		System.out.println("\n\nTesting Splay");
-		test3(splay, 100, 5, 1000);
-	}
-	
-	void test1(SearchTree st) {
+	void test1(SearchTree st, SearchTree spl) {
+		System.out.println("starting test 1 ---");
 		int count = 0;
 		while (count< 100) {
 			int num = gen.nextInt(100);
@@ -42,15 +47,21 @@ class BasicTests {
 		for(int i =0; i<100;i++) {
 			Node inst = new Node(exp.get(i));
 			st.addNode(inst);
+			spl.addNode(inst);
 		}
 		for(int i =0;i<100;i++) {
 			int n2 = gen.nextInt(100);
 			st.searchNode(n2);
-			System.out.println("node "+n2+" access count "+st.getAcessCount());
+			if(i%10==0) {
+			System.out.println("node"+n2+"access count "+st.getAcessCount());
 		}
-		assertEquals(st.searchNode(2), true);
+			System.out.println("");
+			System.out.println("splay node  "+spl.getAcessCount());
+		}
+
 	}
 	void test2(SearchTree st, SearchTree spl) {
+		System.out.println("test 2 start---");
 		int conNumb =0;
 		int count =0;
 		while (count<500) {
@@ -68,81 +79,87 @@ class BasicTests {
 			}else {
 			spl.addNode(inst);
 			st.addNode(inst);
-			System.out.println("add node "+"splay "+spl.getAcessCount()+" bst "+st.getAcessCount());
+			if(i%50==0) {
 		}
+			}
 		}
+		st.clearAcessCount();
+		spl.clearAcessCount();
 		for(int j =0;j<500;j++) {
 			int n3 = gen.nextInt(500);
-			if(j%50==0) {
+			if(j%5==0) {
 				int nCon = 35;
 				st.searchNode(nCon);
 				spl.searchNode(nCon);
-				System.out.println("     control node "+nCon+" get access count of control for bst "+st.getAcessCount()
-				+" splay access count "+spl.getAcessCount());
-				
+				System.out.println("control node "+nCon+"get access count of control for bst "+st.getAcessCount()
+				+"splay access count "+spl.getAcessCount());
+				System.out.println("");
 			}else {
 			st.searchNode(n3);
 			spl.searchNode(n3);
 			}
-			System.out.println("for bst: node "+n3+" access count "+ st.getAcessCount());
-			System.out.println("for splay: node "+n3+" access count "+spl.getAcessCount());
-			
+			if(j%50==0) {
+			System.out.println("for BST: node "+n3+"access count "+ st.getAcessCount());
+			System.out.println("for splay: node "+n3+"access count "+spl.getAcessCount());
+			}
 		}
 	}
 	
-	/*
-	 * 
-	 * int elements The number of items to add
-	 * int subElements The number of sub elements to add
-	 * int searches The number of searches to perform
-	 * 
-	 * This test focuses on random data with a small set of values to search for
-	 * 'element' number of  nodes will be inserted into the tree and 'subElements' number of nodes will be chosen to be the set to search for. 
-	 * The subElements will be added linearly throughout adding the nodes to the tree
-	 * After inserting all of the nodes, 1 of the N number of nodes will be searched for. This will be repeated 'searches' times
-	 * 
-	 * 
-	 */
-	void test3(SearchTree st, int elements, int subElements, int searches) {
+	void test3(SearchTree st, SearchTree spl) {
+		System.out.println("Test 3 start---");
+		int[] controlTest = new int[] {10, 9, 8, 7, 6, 5, 4, 3, 2,1,0};
+		for(int i = 0; i<controlTest.length;i++){
+			Node ins = new Node(controlTest[i]);
+			st.addNode(ins);
+			spl.addNode(ins);
+		}
 		st.clearAcessCount();
-		
-		int[] subSet = new int[subElements];
-		int counter = 0;
-		
-		int mod = elements/ subElements;
-		
-		// create random with seed of 1 to keep it consistent.
-		Random rand = new Random();
-		for(int i = 0; i< elements;i++) {
-			int add = rand.nextInt();
-			st.addNode(new Node(add));
-			if(i % mod == 0 && counter < subElements) {
-				subSet[counter] = add;
-				counter++;
-			}
-		}
-		
-		System.out.println("Cost to add elements :: Access Count: " + st.getAcessCount());
-		
-		System.out.println("SubSet values:");
-		for(int i = 0;i<subElements;i++) {
-			System.out.print(subSet[i] + ", ");
-		}
-		System.out.println("");
-		
-		st.clearAcessCount();
-		
-		System.out.println("Searching...");
-		
-		for(int i =0;i<searches;i++) {
-			int index = rand.nextInt(0,subElements);
-			int key = subSet[index];
-			st.searchNode(key);
-		}
-		
-		System.out.println("Cost to search elements :: Access Count: " + st.getAcessCount());
-		
-		
-	}
+		spl.clearAcessCount();
+		for(int j =0;j<10;j++){
+			st.searchNode(0);
+			spl.searchNode(0);
+			System.out.println("access count for bst: "+st.getAcessCount());
+			System.out.println("access count for splay: "+spl.getAcessCount());
 
+		}
+	}
+	void test4(SearchTree st, SearchTree spl){
+		System.out.println("test 4 start");
+		int[] testarr = new int[]{13, 12, 14, 11, 15, 9, 16, 8, 17, 7};
+		for(int i = 0; i<testarr.length;i++){
+			Node ins = new Node(testarr[i]);
+			st.addNode(ins);
+			spl.addNode(ins);
+		}
+		for(int j =0; j<100;j++){
+			int ran = gen.nextInt(13);
+			st.searchNode(ran);
+			spl.searchNode(ran);
+			if(j%10==0) {
+			System.out.println("access count for bst: "+st.getAcessCount());
+			System.out.println("access count for splay: "+spl.getAcessCount());
+		}
+		}
+	}
+	void test5(SearchTree st, SearchTree spl) {
+		System.out.println("Test 5 start---");
+		int[] test5Array = new int[100];
+		int[] searchArray = new int[] {30, 27, 86};
+		for(int i = 0; i<test5Array.length;i++) {
+			Node ins = new Node(gen.nextInt(100));
+			st.addNode(ins);
+			spl.addNode(ins);
+		}
+		st.clearAcessCount();
+		spl.clearAcessCount();
+		for(int j = 0; j<50;j++) {
+			int nran = gen.nextInt(2);
+			spl.searchNode(searchArray[nran]);
+			st.searchNode(searchArray[nran]);
+			if(j%5==0) {
+			System.out.println("splay access counts   "+spl.getAcessCount());
+			System.out.println("");
+			System.out.println("bst access counts   "+st.getAcessCount());
+		}}
+	}
 }
